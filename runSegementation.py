773e58daa91c4be3bb1a3035782048be469a1b01
shashank2408 <shashank2408@github.com>
2019-08-18 22:45:57 -0400

    Initial Commit

---
+++ runSegementation.py
@@ -0,0 +1,60 @@
+import rospy 
+import torch
+import os
+import rosparam
+from sensor_msgs.msg import *
+from cv_bridge import CvBridge, CvBridgeError
+import numpy as np
+from utils import *
+import cv2
+
+class RunSegmentation:
+    
+    def __init__(self):
+        imageTopic = rosparam.get_param("image_topic")
+        publishTopic = rosparam.get_param("pub_topic")
+        imageRes= rosparam.get_param("resolution")
+        self.resolution = imageRes.split("x")
+        modelType = rosparam.get_param("model")
+        scale = rosparam.get_param("scale")
+        self.bridge = CvBridge()
+        self.image = np.zeros(self.resolution[0], self.resolution[1],3)
+        self.model = setupSegNet(modelType,scale)
+        self.listener(imageTopic,publishTopic)
+
+
+    def ImageCallback(self,data):
+        try:
+            img = self.bridge.imgmsg_to_cv2(data,"bgr8")
+        except CvBridgeError as e:
+            print(e)
+        img_out = self.evaluate(img)
+        self.pub.publish(img_out)
+
+
+    def evaluate(self,img):
+        img_size = tuple(self.resolution)
+        w, h = cv2.GetSize(img)
+        device = 'cuda' if num_gpus >= 1 else 'cpu'
+        img = data_transfom(img,img_size)
+        img = img.unsqueeze(0)  # add a batch dimension
+        img = img.to(device)
+        img_out = model(img)
+        img_out = img_out.max(0)[1].byte()  # get the label map
+        img_out = img_out.to(device='cpu').numpy()
+        img_out = relabel(img_out)
+        
+        img_out = cv2.resize(img_out, (w, h), interpolation=cv2.INTER_NEAREST)
+
+        return img_out
+
+
+    def listener(self, imageTopic,publishTopic):
+        rospy.init_node("ESPNet_ROS")
+        sub = rospy.Subscriber(imageTopic,Image,ImageCallback)
+        self.pub = rospy.Publisher(publishTopic,Image, queue_size = 10)
+
+
+if __name__ == "__main__":
+    obj = RunSegmentation()
+    rospy.spin()
---
+++ utils.py
@@ -0,0 +1,100 @@
+from model.weight_locations.segmentation import model_weight_map
+import rospy
+from data_loader.segmentation.cityscapes import CITYSCAPE_CLASS_LIST
+from torchvision.transforms import functional as F
+from transforms.classification.data_transforms import MEAN, STD
+
+    def relabel(img):
+        '''
+        This function relabels the predicted labels so that cityscape dataset can process
+        :param img:
+        :return:
+        '''
+        img[img == 19] = 7
+        img[img == 18] = 33
+        img[img == 17] = 32
+        img[img == 16] = 31
+        img[img == 15] = 28
+        img[img == 14] = 27
+        img[img == 13] = 26
+        img[img == 12] = 25
+        img[img == 11] = 24
+        img[img == 10] = 23
+        img[img == 9] = 22
+        img[img == 8] = 21
+        img[img == 7] = 20
+        img[img == 6] = 19
+        img[img == 5] = 17
+        img[img == 4] = 13
+        img[img == 3] = 12
+        img[img == 2] = 11
+        img[img == 1] = 8
+        img[img == 0] = 255
+        img[img == 255] = 0
+        return img
+
+
+    def data_transform(img, im_size):
+        img = img.resize(im_size, Image.BILINEAR)
+        img = F.to_tensor(img)  # convert to tensor (values between 0 and 1)
+        img = F.normalize(img, MEAN, STD)  # normalize the tensor
+        return img
+
+    def getPreTrainedModel(model,scale, im_size):
+        model_key = '{}_{}'.format(args.model, args.s)
+        dataset_key = '{}_{}x{}'.format(args.dataset, args.im_size[0], args.im_size[1])
+        assert model_key in model_weight_map.keys(), '{} does not exist'.format(model_key)
+        assert dataset_key in model_weight_map[model_key].keys(), '{} does not exist'.format(dataset_key)
+        weights_test = model_weight_map[model_key][dataset_key]['weights']
+
+
+        if not os.path.isfile(args.weights_test):
+            print("Weights file not found. Check Params")
+            exit(-1)
+
+        return weights_test
+
+    def setupSegNet(modelType,scale):
+        segClasses = len(CITYSCAPE_CLASS_LIST)
+
+        modelWeightsFile = getPreTrainedModel(modelType,scale,im_size)
+        
+        if modelType == 'espnetv2':
+            model = espNetModel(segClasses,scale,modelWeightsFile)
+        elif modelType == 'dicenet':
+            model = diceNet(modelWeightsFile, segClasses)
+        return model
+
+
+    def espNetModel(classes,scale,weights,dataset="city"):
+        model = ESPNetv2Segmentation(args, classes=classes, dataset=dataset)
+        num_gpus = torch.cuda.device_count()
+        device = 'cuda' if num_gpus >= 1 else 'cpu'
+        pretrained_dict = torch.load(weights, map_location=torch.device(device))
+
+        basenet_dict = model.base_net.state_dict()
+        model_dict = model.state_dict()
+        overlap_dict = {k: v for k, v in pretrained_dict.items() if k in basenet_dict}
+
+        basenet_dict.update(overlap_dict)
+        model.base_net.load_state_dict(basenet_dict)
+
+        rospy.loginfo("Loaded PreTrained weights")
+
+        return model
+
+    def diceNet(weights,classes):
+        model = DiCENetSegmentation(args, classes=classes)
+        num_gpus = torch.cuda.device_count()
+        device = 'cuda' if num_gpus >= 1 else 'cpu'
+        pretrained_dict = torch.load(weights, map_location=torch.device(device))
+
+        basenet_dict = model.base_net.state_dict()
+        model_dict = model.state_dict()
+        overlap_dict = {k: v for k, v in pretrained_dict.items() if k in basenet_dict}
+
+        basenet_dict.update(overlap_dict)
+        model.base_net.load_state_dict(basenet_dict)
+        rospy.loginfo("Loaded PreTrained weights")
+
+        return model
